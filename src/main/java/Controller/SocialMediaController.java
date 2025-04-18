c6e4b663c7a819d25ee90223f335b5052175487c
Jacob McDaniels <jamcdaniels99@gmail.com>
2025-04-18 15:02:44 +0000

    new additions to socialmedia controller for api endpoints

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -2,6 +2,7 @@ package Controller;
 
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+import java.util.List;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -28,6 +29,12 @@ public class SocialMediaController {
         Javalin app = Javalin.create();
         app.post("/accounts", this::postAccountHandler);
         app.get("/accounts", this::verifyUserHandler);
+        app.post("/messages", this::postMessage);
+        app.get("/messages", this::getAllMessagesByUser);
+        app.get("/messages", this::getAllMessages);
+        app.post("/messages", this::deleteMessageHandler);
+        app.post("/messages", this::updateMessageHandler);
+        app.get("/messages", this::getMessagesByMessageID);
 
 
 
@@ -41,6 +48,12 @@ public class SocialMediaController {
 
     private void verifyUserHandler(Context ctx)
     {
+        /*Need to figure out how to make statements to read username and then another statement for password 
+        ObjectMapper mapper = new ObjectMapper();
+        Account account = mapper.readValue(ctx.body());
+        Account verfiyAccount = accountService.verifyAccount(account);
+        */
+        
     
 
     }
@@ -58,6 +71,101 @@ public class SocialMediaController {
         {
             ctx.status(400);
         }
+    }
+
+    private void postMessage(Context ctx)
+    {
+
+    }
+
+    private void getAllMessagesByUser(Context ctx) throws JsonProcessingException
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        
+        int userID = Integer.parseInt(ctx.pathParam("posted_by"));
+        List<Message> userMessages = messageService.getUserMessages(userID);
+
+        System.out.println(userMessages);
+
+        if(userMessages == null)
+        {
+            ctx.status(400);
+
+        }
+        else
+        {
+            ctx.json(mapper.writeValueAsString(userMessages));
+
+        }
+
+    }
+
+    private void getAllMessages(Context ctx)
+    {
+        List<Message> messages = messageService.getAllMessage();
+        ctx.json(messages);
+
+    }
+
+    private void deleteMessageHandler(Context ctx) throws JsonProcessingException
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        
+        int message_id = Integer.parseInt(ctx.pathParam("message_id"));
+        Message deletedMessage = messageService.messageByID(message_id);
+
+        System.out.println(deletedMessage);
+
+        if(deletedMessage == null)
+        {
+            ctx.status(400);
+        }
+        else
+        {
+            ctx.json(mapper.writeValueAsString(deletedMessage));
+            messageService.deleteMessByID(message_id);
+        }
+
+    }
+
+    private void updateMessageHandler(Context ctx) throws JsonProcessingException
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        Message message = mapper.readValue(ctx.body(), Message.class);
+        int message_id = Integer.parseInt(ctx.pathParam("message_id"));
+        Message updatedMessage = messageService.updateMessages((message_id), message);
+        System.out.println(updatedMessage);
+
+        if(updatedMessage == null)
+        {
+            ctx.status(400);
+        }
+        else
+        {
+            ctx.json(mapper.writeValueAsString(updatedMessage));
+        }
+
+
+    }
+
+    private void getMessagesByMessageID(Context ctx) throws JsonProcessingException
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        int message_id = Integer.parseInt(ctx.pathParam("message_id"));
+
+        Message messagesByMessID = messageService.messageByID(message_id);
+        System.out.println(messagesByMessID);
+
+        if(messagesByMessID == null)
+        {
+            ctx.status(400);
+        }
+        else
+        {
+            ctx.json(mapper.writeValueAsString(messagesByMessID));
+        }
+        
+
     }
 
 
